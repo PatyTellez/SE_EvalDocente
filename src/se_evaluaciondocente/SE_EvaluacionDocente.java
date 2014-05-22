7db1a227ac4797b343f0fd406bf6992b94edf9bf
root <root@>
2014-05-22 16:57:17 -0500

    FAMA

--- src/se_evaluaciondocente/SE_EvaluacionDocente.java
+++ src/se_evaluaciondocente/SE_EvaluacionDocente.java
@@ -3,6 +3,7 @@
 
 package se_evaluaciondocente;
 
+import difusificar.Difusificar;
 import java.io.IOException;
 
 public class SE_EvaluacionDocente {
@@ -18,6 +19,10 @@ public class SE_EvaluacionDocente {
         RW_FAM fbin = new RW_FAM();
         fbin.escribir_bin();
         fbin.leer_bin();
+        
+        Difusificar d = new Difusificar();
+        fbin.leer_bin();
+        
         
     }
     
